23b21184b4ad4897b4b3a2b3428a149e8a493459
dwainejade <dwainegnd@gmail.com>
2023-12-25 17:20:30 -0500

    remove suspense. added dof

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React, { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-// import { Perf } from "r3f-perf";
+import { Perf } from "r3f-perf";
 import Scene from "./Scene";
-import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { Bloom, DepthOfField, EffectComposer } from "@react-three/postprocessing";
 import Menu from "./components/Menu";
+import { usePlanetStore } from "./store/store";
 
 const App = () => {
+  const { selectedPlanet } = usePlanetStore();
+
   return (
     <div className='Main'>
-      <Canvas id='Canvas' dpr={[1, 2]} camera={{ fov: 45, position: [40, 40, 40], near: 0.1, far: 2000 }}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Scene />
-        </Suspense>
+      <Canvas id='Canvas' dpr={[1, 2]} camera={{ fov: selectedPlanet ? 25 : 50, position: [5000, 5000, 5000], near: 0.1, far: 100000 }}>
+        {/* <Suspense fallback={<div>Loading...</div>}> */}
+        <Scene />
+        {/* </Suspense> */}
+        {/* <Perf /> */}
         <EffectComposer>
           <Bloom mipmapBlur intensity={0.3} luminanceThreshold={1} luminanceSmoothing={1.2} radius={0.6} />
+          <DepthOfField focusDistance={selectedPlanet ? 4 : 0} focalLength={8} bokehScale={3} />
         </EffectComposer>
       </Canvas>
       <Menu />
