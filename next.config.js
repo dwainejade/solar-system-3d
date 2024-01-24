f2d400b4a172e50ae654885b0ee6fd8a652f1a52
dwainejade <dwainegnd@gmail.com>
2024-01-24 17:20:41 -0500

    update config for exporting static files

--- next.config.js
+++ next.config.js
@@ -17,6 +17,13 @@ const nextConfig = {
   // compiler: {
   //   styledComponents: true,
   // },
+  output: 'export',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: 'dist',
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   images: {},
   webpack(config, { isServer }) {
