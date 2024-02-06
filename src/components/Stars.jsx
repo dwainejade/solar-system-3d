cb5d3549e9ebcf0711b406d6513fdb7c7c6ca180
dwainejade <dwainegnd@gmail.com>
2024-02-06 15:33:57 -0500

    fixed constellation warpping

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,26 +1,46 @@
-import { useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
-import { BackSide } from "three";
-import useStore from "../store/store";
+import { useEffect } from 'react';
+import { useLoader, useThree } from '@react-three/fiber';
+import { CubeTextureLoader } from 'three';
+import { BackSide } from 'three';
+import useStore from '../store/store';
+
+
+
+function SkyBox({ texture, showConstellations }) {
+  const { scene } = useThree();
+
+  useEffect(() => {
+    scene.background = texture;
+  }, [showConstellations]);
+  return null;
+}
 
 const Stars = () => {
-  const starsTexture = useLoader(TextureLoader, "../assets/stars/starmap_8k.png");
-  const constellationTexture = useLoader(TextureLoader, "../assets/stars/constellations.png");
   const { showConstellations } = useStore();
-  const size = 100000;
+  const loader = new CubeTextureLoader();
+
+  const starsTexture = loader.load([
+    '../assets/stars/stars-cube-map/px.png',
+    '../assets/stars/stars-cube-map/nx.png',
+    '../assets/stars/stars-cube-map/py.png',
+    '../assets/stars/stars-cube-map/ny.png',
+    '../assets/stars/stars-cube-map/pz.png',
+    '../assets/stars/stars-cube-map/nz.png',
+  ]);
+  const constellationsTexture = loader.load([
+    '../assets/stars/constellations-cube-map/px.png',
+    '../assets/stars/constellations-cube-map/nx.png',
+    '../assets/stars/constellations-cube-map/py.png',
+    '../assets/stars/constellations-cube-map/ny.png',
+    '../assets/stars/constellations-cube-map/pz.png',
+    '../assets/stars/constellations-cube-map/nz.png',
+  ]);
+
   return (
     <>
-      <mesh>
-        <sphereGeometry args={[size + 1, 128]} />
-        <meshBasicMaterial map={starsTexture} side={BackSide} />
-      </mesh>
-
-      {showConstellations && (
-        <mesh>
-          <sphereGeometry args={[size, 128]} />
-          <meshBasicMaterial map={constellationTexture} transparent={true} opacity={0.2} side={BackSide} />
-        </mesh>
-      )}
+      <SkyBox
+        showConstellations={showConstellations}
+        texture={showConstellations ? constellationsTexture : starsTexture} />
     </>
   );
 };
--- src/components/canvas/SharedCanvas.jsx
+++ src/components/canvas/SharedCanvas.jsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { Suspense, useEffect } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import Stars from "../Stars";
-import { Html, Preload, Stats, useProgress } from "@react-three/drei";
+import { Environment, Html, Preload, Stats, useProgress } from "@react-three/drei";
 import useStore from "../../store/store";
 import Menu from "../Menu";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import "../../styles.css";
+import { RGBADepthPacking } from "three";
 
 const SharedCanvas = ({ children }) => {
   const { fullscreen } = useStore();
@@ -33,15 +34,39 @@ const SharedCanvas = ({ children }) => {
       </Html>
     );
   };
-
+  const [constellations, setConstellations] = useState(false)
+  const toggleStars = () => {
+    console.log('toggled stars', !constellations)
+    setConstellations(!constellations)
+  }
   return (
     <div className={`Main ${fullscreen ? "fullscreen" : "minimized"}`}>
-      <Canvas id='Canvas' dpr={[1, 2]} camera={{ fov: 50, position: [5000, 5000, 5000], near: 0.01, far: 200000 }}>
+      <Canvas id='Canvas' dpr={[1, 2]} camera={{ fov: 50, position: [5000, 5000, 5000], near: 0.1, far: 200000 }} onAuxClick={toggleStars}>
         <Suspense fallback={<Loader />}>
           <Stats />
-          <Stars />
+          {/* <Stars /> */}
 
-          <ambientLight intensity={0.1} />
+          {/* <Environment
+            background="only"
+            blur={0}
+            files={['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']}
+            path={`../assets/stars/stars-cube-map/`}
+            preset={null}
+            scene={undefined}
+          /> */}
+
+          {/* <Environment
+            background="only"
+            blur={0}
+            files={['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']}
+            path="../assets/stars/constellations-cube-map/"
+            preset={null}
+            scene={undefined}
+            encoding={undefined}
+          // ground={{ radius: 50000, scale: 1000 }}
+          /> */}
+
+          <ambientLight intensity={0.04} />
           <pointLight color='#f6f3ea' intensity={1} position={[0, 0, 0]} />
           <EffectComposer>
             <Bloom mipmapBlur intensity={0.3} luminanceThreshold={1} luminanceSmoothing={1.2} radius={0.6} />
