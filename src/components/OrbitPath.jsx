314563e97bcf315006c75363735fec09831c2460
dwainejade <dwainegnd@gmail.com>
2024-03-05 14:14:06 -0500

    adjust orbit line width

--- src/components/OrbitPath.jsx
+++ src/components/OrbitPath.jsx
@@ -18,7 +18,7 @@ const OrbitPath = forwardRef(
       return pathPoints;
     }, [origin, radius]);
 
-    return <Line ref={ref} points={points} color={color} lineWidth={0.2} />;
+    return <Line ref={ref} points={points} color={color} lineWidth={0.25} />;
   }
 );
 
