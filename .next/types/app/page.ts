2a21f76c84fc7b9d17f2e6f57da2b315e7e3205d
Dwaine Matthew <dwainejade@gmail.com>
2024-04-23 17:23:15 -0400

    new build

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/dmatthew8972/Desktop/personal/solar-sim/app/page.jsx
+// File: /Users/dmatthew8972/Desktop/strongmind/github/solar-system-interactive/app/page.jsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
