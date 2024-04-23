f6c02507f34d2a2d543918f71c92ee8981fbc2c9
Dwaine Matthew <dwainejade@gmail.com>
2024-04-23 14:48:36 -0400

    remove unused code

--- src/components/canvas/SharedCanvas.jsx
+++ src/components/canvas/SharedCanvas.jsx
@@ -6,8 +6,7 @@ import { Html, Preload, Stats, useProgress } from "@react-three/drei";
 import useStore from "../../store/store";
 import Menu from "../UI/Menu";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
-// import { BlendFunction } from "postprocessing";
-// import ContextMenu from "../ContextMenu";
+
 import "../../styles.css";
 
 const SharedCanvas = ({ children }) => {
@@ -16,28 +15,6 @@ const SharedCanvas = ({ children }) => {
   const { errors, loaded } = useProgress();
   const totalAssets = 12;
   const progressPercentage = (loaded / totalAssets) * 100;
-  // const [contextMenu, setContextMenu] = useState({ visible: false, x: 0, y: 0 });
-
-  // const handleAuxClick = (event) => {
-  //   event.preventDefault();
-  //   setContextMenu({
-  //     visible: true,
-  //     x: event.clientX,
-  //     y: event.clientY,
-  //   });
-  // };
-
-  // const hideContextMenu = () => {
-  //   setContextMenu({ ...contextMenu, visible: false });
-  // };
-
-  // // Listen for clicks to hide the context menu if visible
-  // useEffect(() => {
-  //   document.addEventListener('click', hideContextMenu);
-  //   return () => {
-  //     document.removeEventListener('click', hideContextMenu);
-  //   };
-  // }, []);
 
   useEffect(() => {
     if (errors.length) {
@@ -62,9 +39,8 @@ const SharedCanvas = ({ children }) => {
     <div className={`Main ${fullscreen ? "fullscreen" : "minimized"}`}>
       <Canvas
         id='Canvas'
-        frameloop="demand"
         shadows dpr={[1, 2]}
-        //  gl={{ antialias: true }} 
+        gl={{ antialias: true }}
         camera={{ fov: 50, position: [5000, 5000, 5000], near: 0.01, far: 1000000 }}
       >
         <Suspense fallback={<Loader />}>
@@ -73,14 +49,12 @@ const SharedCanvas = ({ children }) => {
           <pointLight color='#f6f3ea' intensity={2} position={[0, 0, 0]} />
           <EffectComposer>
             <Bloom mipmapBlur intensity={.6} luminanceThreshold={1} luminanceSmoothing={1.2} radius={.6} />
-            {/* <Noise premultiply blendFunction={BlendFunction.ADD} /> */}
           </EffectComposer>
 
           {children}
         </Suspense>
         <Preload all />
       </Canvas>
-      {/* {contextMenu.visible && <ContextMenu x={contextMenu.x} y={contextMenu.y} onResetView={resetCamera} />} */}
       <Menu />
     </div>
   );
