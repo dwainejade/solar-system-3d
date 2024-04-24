36cdb35da6cb994206b51958cca18bf90b2dca95
Dwaine Matthew <dwainejade@gmail.com>
2024-04-24 14:07:05 -0400

    using hires lines when planet is selected

--- src/components/OrbitPath.jsx
+++ src/components/OrbitPath.jsx
@@ -3,20 +3,22 @@ import { Line } from "@react-three/drei";
 import * as THREE from "three";
 
 const OrbitPath = forwardRef(
-  ({ origin = new THREE.Vector3(0, 0, 0), radius = 2, color = "white", name = "orbit-path", orbitalInclination }, ref) => {
+  ({ origin = new THREE.Vector3(0, 0, 0), radius = 2, color = "white", name = "orbit-path", orbitalInclination, hiRes = false }, ref) => {
     // Convert inclination to radians
     const inclination = orbitalInclination * (Math.PI / 180);
 
     const points = useMemo(() => {
       const pathPoints = [];
-      for (let angle = 0; angle < Math.PI * 2; angle += Math.PI / 150) {
+      const resolution = hiRes ? Math.min(radius / 2, 1000) : 30;
+
+      for (let angle = 0; angle < Math.PI * 2; angle += Math.PI / resolution) {
         const x = radius * Math.cos(angle);
         const y = Math.sin(inclination) * radius * Math.sin(angle);
         const z = radius * Math.sin(angle);
         pathPoints.push(new THREE.Vector3(x, y, z));
       }
       return pathPoints;
-    }, [origin, radius]);
+    }, [origin, radius, hiRes]);
 
     return <Line ref={ref} points={points} color={color} lineWidth={0.25} />;
   }
