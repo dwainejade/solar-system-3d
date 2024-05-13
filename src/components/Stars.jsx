b579b4dde4eabe9c96aabef06d5870099fb53ff0
Dwaine Matthew <dwainejade@gmail.com>
2024-05-13 16:41:50 -0400

    fix stars loading

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,39 +1,49 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useThree } from '@react-three/fiber';
 import { CubeTextureLoader } from 'three';
 import useStore from '../store/store';
 
-
-
-function SkyBox({ texture, showConstellations }) {
+function SkyBox({ texture }) {
   const { scene } = useThree();
 
   useEffect(() => {
     scene.background = texture;
-  }, [showConstellations]);
+  }, [texture]);
+
   return null;
 }
 
 const Stars = () => {
   const { showConstellations } = useStore();
-  const loader = new CubeTextureLoader();
+  const [starsTexture, setStarsTexture] = useState();
 
-  const starsTexture = loader.load([
-    '../assets/stars/stars-cube-map/px.png',
-    '../assets/stars/stars-cube-map/nx.png',
-    '../assets/stars/stars-cube-map/py.png',
-    '../assets/stars/stars-cube-map/ny.png',
-    '../assets/stars/stars-cube-map/pz.png',
-    '../assets/stars/stars-cube-map/nz.png',
-  ]);
-  // const constellationsTexture = loader.load([
-  //   '../assets/stars/constellations-cube-map/px.png',
-  //   '../assets/stars/constellations-cube-map/nx.png',
-  //   '../assets/stars/constellations-cube-map/py.png',
-  //   '../assets/stars/constellations-cube-map/ny.png',
-  //   '../assets/stars/constellations-cube-map/pz.png',
-  //   '../assets/stars/constellations-cube-map/nz.png',
-  // ]);
+  useEffect(() => {
+    const loader = new CubeTextureLoader();
+    const texture = loader.load([
+      '../assets/stars/stars-cube-map/px.png',
+      '../assets/stars/stars-cube-map/nx.png',
+      '../assets/stars/stars-cube-map/py.png',
+      '../assets/stars/stars-cube-map/ny.png',
+      '../assets/stars/stars-cube-map/pz.png',
+      '../assets/stars/stars-cube-map/nz.png',
+    ]);
+
+    const constellationsTexture = loader.load([
+      '../assets/stars/constellations-cube-map/px.png',
+      '../assets/stars/constellations-cube-map/nx.png',
+      '../assets/stars/constellations-cube-map/py.png',
+      '../assets/stars/constellations-cube-map/ny.png',
+      '../assets/stars/constellations-cube-map/pz.png',
+      '../assets/stars/constellations-cube-map/nz.png',
+    ]);
+
+    setStarsTexture(showConstellations ? constellationsTexture : texture);
+
+    return () => {
+      texture.dispose();
+      constellationsTexture.dispose();
+    };
+  }, [showConstellations]);
 
   return (
     <>
