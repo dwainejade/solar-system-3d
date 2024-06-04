3fc58e6371ffe1caafe4d5606b0a53f3ffcb4fe8
Michael Takeuchi <mikemtaki@gamil.com>
2024-06-04 12:36:08 -0700

    add iframe resizer

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 export const metadata = {
   title: 'Solar System',
   description: 'Solar System Simulator',
@@ -6,6 +8,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        key='https://code.jquery.com/jquery-3.2.1.slim.min.js'
+        src='https://code.jquery.com/jquery-3.2.1.slim.min.js'
+        integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN'
+        crossOrigin='anonymous'
+        defer
+      />
+      <Script
+        key='https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.2.11/iframeResizer.contentWindow.min.js'
+        src='https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.2.11/iframeResizer.contentWindow.min.js'
+        integrity='sha256-EH+7IdRixWtW5tdBwMkTXL+HvW5tAqV4of/HbAZ7nEc='
+        crossOrigin='anonymous'
+      />
+      
       <body>{children}</body>
     </html>
   )
---
+++ dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aLnQsD5iZL93fexBD4z87HhxuEWAQmTgrB4uPUxBFPE=\"\n}"
