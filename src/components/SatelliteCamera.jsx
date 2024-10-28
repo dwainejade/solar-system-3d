772f9b0b3e47a0346f5dbe52d82893f58d27052d
Dwaine Matthew <dwainejade@gmail.com>
2024-10-28 15:56:01 -0400

    smoother transition to planet camera

--- src/components/SatelliteCamera.jsx
+++ src/components/SatelliteCamera.jsx
@@ -1,27 +1,33 @@
-import React, { useRef, useState, useEffect, useCallback, useLayoutEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 import { useThree, useFrame } from '@react-three/fiber';
 import { PerspectiveCamera } from '@react-three/drei';
 import * as THREE from 'three';
 import useStore, { useCameraStore } from '../store/store';
 
-
-const SatelliteCamera = ({ target, size, satelliteCamera, toggleSatelliteCamera, targetName }) => {
-    const { toggleCameraTransitioning, setAutoRotate, autoRotate, toggleOrbitCamera, setSatelliteCameraState } = useCameraStore();
+const SatelliteCamera = ({ target, size, targetName, parentTransform = null }) => {
+    const {
+        toggleCameraTransitioning,
+        setAutoRotate,
+        autoRotate,
+        activeCamera,
+        toggleSatelliteCamera,
+        satelliteCamera,
+    } = useCameraStore();
     const { prevSpeed, setSimSpeed } = useStore();
-
-    const { gl } = useThree();
+    const { gl, camera: orbitCamera } = useThree();
     const cameraRef = useRef();
 
     const [isDragging, setIsDragging] = useState(false);
     const [mouseDownPosition, setMouseDownPosition] = useState({ x: 0, y: 0 });
-    const [spherical, setSpherical] = useState(new THREE.Spherical());
+    const [spherical, setSpherical] = useState(
+        new THREE.Spherical(size * 6, Math.PI / 2, 0)
+    );
 
-    const [targetRadius, setTargetRadius] = useState(size * 5.5);
-    const lerpFactor = 0.18; // Adjust this value to control the smoothness of the transition
+    const lerpFactor = 0.18; // Adjust to control smoothness
 
     const handleUserInteraction = useCallback(() => {
-        setAutoRotate(false)
-    }, []);
+        setAutoRotate(false);
+    }, [setAutoRotate]);
 
     const handleMouseDown = useCallback((event) => {
         setIsDragging(true);
@@ -30,7 +36,6 @@ const SatelliteCamera = ({ target, size, satelliteCamera, toggleSatelliteCamera,
 
     const handleMouseUp = useCallback(() => {
         setIsDragging(false);
-        setSpherical((prevSpherical) => new THREE.Spherical().copy(prevSpherical));
     }, []);
 
     const handleMouseMove = useCallback(
@@ -40,7 +45,7 @@ const SatelliteCamera = ({ target, size, satelliteCamera, toggleSatelliteCamera,
                 const dx = event.clientX - mouseDownPosition.x;
                 const dy = event.clientY - mouseDownPosition.y;
 
-                const dampingFactor = .4; // Adjust this value to control damping (0 for no damping, 1 for no movement)
+                const dampingFactor = 0.4;
 
                 setSpherical((prevSpherical) => {
                     const newSpherical = new THREE.Spherical(
@@ -48,7 +53,10 @@ const SatelliteCamera = ({ target, size, satelliteCamera, toggleSatelliteCamera,
                         prevSpherical.phi - dy * dampingFactor * 0.01,
                         prevSpherical.theta - dx * dampingFactor * 0.01
                     );
-                    newSpherical.phi = Math.max(0.1, Math.min(Math.PI - 0.1, newSpherical.phi));
+                    newSpherical.phi = Math.max(
+                        0.1,
+                        Math.min(Math.PI - 0.1, newSpherical.phi)
+                    );
 
                     return newSpherical;
                 });
@@ -56,63 +64,74 @@ const SatelliteCamera = ({ target, size, satelliteCamera, toggleSatelliteCamera,
                 setMouseDownPosition({ x: event.clientX, y: event.clientY });
             }
         },
-        [isDragging, mouseDownPosition]
+        [isDragging, mouseDownPosition, handleUserInteraction]
     );
-
 
     const handleWheel = useCallback(
         (event) => {
             if (!satelliteCamera) return;
 
-            const currentPosition = new THREE.Vector3().setFromSpherical(spherical);
-            const distanceToTarget = currentPosition.distanceTo(target.position);
-
-            // Calculate the zoom factor based on the distance to target
-            const zoomFactor = Math.max(0.1, 1 / (distanceToTarget / 500)); // Adjust the scaling factor as needed
-
-            // Calculate the new target radius based on the wheel event and the zoom factor
-            const zoomSpeed = -0.2; // Adjust this value to control the zoom speed
-            const deltaRadius = event.deltaY * zoomSpeed * zoomFactor;
-            const newTargetRadius = Math.max(size * 2.5, Math.min(500, targetRadius - deltaRadius));
-
-            setTargetRadius(newTargetRadius);
+            const zoomSpeed = 0.01;
+            const deltaRadius = event.deltaY * zoomSpeed;
+            setSpherical((prevSpherical) => {
+                const newRadius = THREE.MathUtils.clamp(
+                    prevSpherical.radius + deltaRadius,
+                    size * 2.5,
+                    500
+                );
+                return new THREE.Spherical(
+                    newRadius,
+                    prevSpherical.phi,
+                    prevSpherical.theta
+                );
+            });
             handleUserInteraction();
         },
-        [spherical, target.position, size, targetRadius]
+        [satelliteCamera, size, handleUserInteraction]
     );
 
-    // Handle touch events
+    // Touch events
     const handleStart = useCallback((event) => {
         setIsDragging(true);
-        // Check if it's a touch event and prevent default behavior to avoid scrolling and zooming interfaces
         if (event.touches) {
             event.preventDefault();
-            setMouseDownPosition({ x: event.touches[0].clientX, y: event.touches[0].clientY });
+            setMouseDownPosition({
+                x: event.touches[0].clientX,
+                y: event.touches[0].clientY,
+            });
         } else {
             setMouseDownPosition({ x: event.clientX, y: event.clientY });
         }
     }, []);
-    const handleMove = useCallback((event) => {
-        if (isDragging) {
-            handleUserInteraction();
-            const clientX = event.touches ? event.touches[0].clientX : event.clientX;
-            const clientY = event.touches ? event.touches[0].clientY : event.clientY;
-            const dx = clientX - mouseDownPosition.x;
-            const dy = clientY - mouseDownPosition.y;
 
-            setSpherical((prevSpherical) => {
-                const newSpherical = new THREE.Spherical(
-                    prevSpherical.radius,
-                    prevSpherical.phi - dy * 0.003,
-                    prevSpherical.theta - dx * 0.003
-                );
-                newSpherical.phi = Math.max(0.1, Math.min(Math.PI - 0.1, newSpherical.phi));
-                return newSpherical;
-            });
+    const handleMove = useCallback(
+        (event) => {
+            if (isDragging) {
+                handleUserInteraction();
+                const clientX = event.touches ? event.touches[0].clientX : event.clientX;
+                const clientY = event.touches ? event.touches[0].clientY : event.clientY;
+                const dx = clientX - mouseDownPosition.x;
+                const dy = clientY - mouseDownPosition.y;
 
-            setMouseDownPosition({ x: clientX, y: clientY });
-        }
-    }, [isDragging, mouseDownPosition]);
+                setSpherical((prevSpherical) => {
+                    const newSpherical = new THREE.Spherical(
+                        prevSpherical.radius,
+                        prevSpherical.phi - dy * 0.003,
+                        prevSpherical.theta - dx * 0.003
+                    );
+                    newSpherical.phi = Math.max(
+                        0.1,
+                        Math.min(Math.PI - 0.1, newSpherical.phi)
+                    );
+                    return newSpherical;
+                });
+
+                setMouseDownPosition({ x: clientX, y: clientY });
+            }
+        },
+        [isDragging, mouseDownPosition, handleUserInteraction]
+    );
+
     const handleEnd = useCallback(() => {
         setIsDragging(false);
     }, []);
@@ -124,21 +143,22 @@ const SatelliteCamera = ({ target, size, satelliteCamera, toggleSatelliteCamera,
         window.addEventListener('mousemove', handleMouseMove);
         window.addEventListener('mouseup', handleMouseUp);
         canvasElement.addEventListener('wheel', handleWheel, { passive: true });
+
         // Touch events
         canvasElement.addEventListener('touchstart', handleStart);
         canvasElement.addEventListener('touchmove', handleMove);
         canvasElement.addEventListener('touchend', handleEnd);
-        // add event listener for panning with right mouse button
+
         canvasElement.addEventListener('contextmenu', (event) => {
             event.preventDefault();
         });
-
 
         return () => {
             canvasElement.removeEventListener('mousedown', handleMouseDown);
             window.removeEventListener('mousemove', handleMouseMove);
             window.removeEventListener('mouseup', handleMouseUp);
             canvasElement.removeEventListener('wheel', handleWheel);
+
             // Touch events
             canvasElement.removeEventListener('touchstart', handleStart);
             canvasElement.removeEventListener('touchmove', handleMove);
@@ -147,81 +167,94 @@ const SatelliteCamera = ({ target, size, satelliteCamera, toggleSatelliteCamera,
             canvasElement.removeEventListener('contextmenu', (event) => {
                 event.preventDefault();
             });
-
         };
-    }, [handleMouseDown, handleMouseMove, handleMouseUp, handleWheel, gl.domElement]);
-
+    }, [
+        handleMouseDown,
+        handleMouseMove,
+        handleMouseUp,
+        handleWheel,
+        handleStart,
+        handleMove,
+        handleEnd,
+        gl.domElement,
+    ]);
 
     useEffect(() => {
         return () => {
             if (satelliteCamera) {
                 toggleSatelliteCamera(false);
-                toggleOrbitCamera(true);
             }
         };
-    }, [satelliteCamera]);
+    }, [satelliteCamera, toggleSatelliteCamera]);
 
-
-    // Lerp the spherical radius towards the target radius
     useFrame((state, delta) => {
-        if (cameraRef.current && target) {
-            const newRadius = THREE.MathUtils.lerp(spherical.radius, targetRadius, lerpFactor);
-            setSpherical((prevSpherical) => new THREE.Spherical(newRadius, prevSpherical.phi, prevSpherical.theta));
+        if (!cameraRef.current || !target) return;
 
-            // setSatelliteCameraState({
-            //     position: cameraRef.current.position.clone(),
-            //     rotation: cameraRef.current.rotation.clone(),
-            //     targetPosition: target.position.clone(),
-            // });
+        // Get the target's world position
+        const targetWorldPos = new THREE.Vector3();
+        target.getWorldPosition(targetWorldPos);
 
+        // Calculate target position in parent's space
+        const targetLocalPos = target.position.clone();
+
+        // Auto-rotate if enabled
+        if (autoRotate) {
+            const rotationSpeed = 0.05 * delta;
+            setSpherical((prevSpherical) => {
+                return new THREE.Spherical(
+                    prevSpherical.radius,
+                    prevSpherical.phi,
+                    prevSpherical.theta + rotationSpeed
+                );
+            });
         }
-        if (autoRotate && cameraRef.current && target) {
-            const rotationSpeed = 0.05 * delta;  // Slow rotation speed
-            spherical.theta += rotationSpeed;
-        }
-    });
 
-    useFrame(({ camera }) => {
-        if (cameraRef.current && target) {
-            const newPosition = new THREE.Vector3().setFromSpherical(spherical);
-            cameraRef.current.position.set(
-                newPosition.x + target.position.x,
-                newPosition.y + target.position.y,
-                newPosition.z + target.position.z
-            );
-            cameraRef.current.lookAt(target.position); // need world position if moon
-            cameraRef.current.updateMatrixWorld();
+        // Calculate new camera position relative to moon in parent space
+        const localOffset = new THREE.Vector3().setFromSpherical(spherical);
+        const newPositionLocal = localOffset.add(targetLocalPos);
 
-            const distance = camera.position.distanceTo(target.position); // need world position if moon
-            if (distance <= size * 5.4) {
-                if (!satelliteCamera) {
-                    switchCamera(camera);
-                }
+        // Set camera position
+        cameraRef.current.position.copy(newPositionLocal);
+        cameraRef.current.lookAt(targetLocalPos);
+        cameraRef.current.updateMatrixWorld();
+
+        // Check if camera should switch
+        if (activeCamera.name === targetName && !satelliteCamera) {
+            const distance = orbitCamera.position.distanceTo(targetWorldPos);
+            const sizeThreshold = Math.max(size * 4.3, 0.01);
+            if (distance <= sizeThreshold) {
+                switchCamera(orbitCamera, targetWorldPos);
             }
         }
     });
 
-    function switchCamera(camera) {
-        const relativePosition = new THREE.Vector3().subVectors(
-            camera.position,
-            target.position
-        );
-        const sphericalPosition = convertVectorToSpherical(relativePosition)
-        cameraRef.current.updateProjectionMatrix();
+    const switchCamera = (orbitCamera, targetWorldPos) => {
+        // Get the orbit camera's world position
+        const orbitCameraPosition = orbitCamera.position.clone();
 
+        // Convert to local space relative to the planet
+        const planetMatrix = target.parent.matrixWorld.clone();
+        const planetMatrixInverse = planetMatrix.invert();
+        const orbitCameraLocal = orbitCameraPosition.applyMatrix4(planetMatrixInverse);
+
+        // Calculate relative position to moon in parent's space
+        const moonLocalPos = target.position.clone();
+        const relativePosition = orbitCameraLocal.sub(moonLocalPos);
+
+        // Convert to spherical coordinates
+        const sphericalPosition = new THREE.Spherical().setFromVector3(relativePosition);
         setSpherical(sphericalPosition);
+
+        // Set initial camera position
+        cameraRef.current.position.copy(orbitCameraLocal);
+        cameraRef.current.lookAt(moonLocalPos);
+
+        // Activate the satellite camera
         toggleSatelliteCamera(true);
         toggleCameraTransitioning(false);
         setSimSpeed(prevSpeed);
     };
-
-    function convertVectorToSpherical(vector) {
-        const spherical = new THREE.Spherical();
-        spherical.setFromVector3(vector);
-        return spherical;
-    }
-
-
+    // console.log(cameraRef.current)
     return (
         <PerspectiveCamera
             ref={cameraRef}
