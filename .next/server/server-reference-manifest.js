b6afde85014eaba358c2d6333b0b91db19a21238
Dwaine Matthew <dwainejade@gmail.com>
2024-10-31 14:03:06 -0400

    new build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Ve9/wwcLujKwaaSVRMEpZiibt4t4u+v0PHDCH7/T1l0=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"T/8viAEE94u5FDThzpNEqF6p/ZinC2JAn7DngN96ZW8=\"}"
