fa4e99d335d77a349076b98f79f8a9a32d426efe
Dwaine Matthew <dwainejade@gmail.com>
2024-10-31 15:50:15 -0400

    conditional for onClick in Sun

--- src/components/Sun.jsx
+++ src/components/Sun.jsx
@@ -9,7 +9,7 @@ import { sunOuterShader } from "../shaders/atmosphere";
 const Sun = ({ position, textures }) => {
   const { selectedPlanet, setSelectedPlanet, planetsData } = usePlanetStore();
   const { toggleZoomingToSun, switchToSunCamera } = useCameraStore();
-  const { experimentMode, toggleExperimentMode } = useExperimentsStore();
+  const { experimentMode } = useExperimentsStore();
   const [isDragging, setIsDragging] = useState(false);
   const initialClickPosition = useRef({ x: 0, y: 0 });
   const { simSpeed } = useStore();
