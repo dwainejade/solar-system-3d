351e4fdfa6e847643732de5ea4d78a2eaa2d75e6
Dwaine Matthew <dwainejade@gmail.com>
2024-11-25 16:51:56 -0500

    add resizer to head

--- app/layout.js
+++ app/layout.js
@@ -7,21 +7,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <head>
         <script
-          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-          crossOrigin="anonymous"
+          key='https://code.jquery.com/jquery-3.2.1.slim.min.js'
+          src='https://code.jquery.com/jquery-3.2.1.slim.min.js'
+          integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN'
+          crossOrigin='anonymous'
           defer
-        ></script>
+        />
         <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.2.11/iframeResizer.contentWindow.min.js"
-          integrity="sha256-EH+7IdRixWtW5tdBwMkTXL+HvW5tAqV4of/HbAZ7nEc="
-          crossOrigin="anonymous"
+          key='https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.2.11/iframeResizer.contentWindow.min.js'
+          src='https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.2.11/iframeResizer.contentWindow.min.js'
+          integrity='sha256-EH+7IdRixWtW5tdBwMkTXL+HvW5tAqV4of/HbAZ7nEc='
+          crossOrigin='anonymous'
         ></script>
+      </head>
+      <body>
         {children}
       </body>
-      
+
     </html>
   )
 }
