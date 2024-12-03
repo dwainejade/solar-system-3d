e06f7a609a43b5d79aeda61d4aefacfda6b46414
Dwaine Matthew <dwainejade@gmail.com>
2024-12-03 13:07:38 -0500

    replaced hyphens with en dashes

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,16 +1,16 @@
 export const getSpeedValue = (key) => {
     switch (key) {
-        case "-1 year /s":
+        case "–1 year /s":
             return -31557600;
-        case "-1 month /s":
+        case "–1 month /s":
             return -2629800;
-        case "-1 week /s":
+        case "–1 week /s":
             return -604800;
-        case "-1 day /s":
+        case "–1 day /s":
             return -86400;
-        case "-1 hour /s":
+        case "–1 hour /s":
             return -3600;
-        case "-1 minute /s":
+        case "–1 minute /s":
             return -60;
         case "real time":
             return 1;
